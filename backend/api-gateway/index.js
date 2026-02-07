24980353644af5ccd7e37db0914a91c6ef46f1e4
arnavmjn007 <arnavmjn007@gmail.com>
2026-02-07 14:30:01 +0545

    Express gateway

--- backend/api-gateway/index.js
+++ backend/api-gateway/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 5000;
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +10,14 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send("Express Gateway is Running!");
 });
+
+app.use('/api', createProxyMiddleware({
+    target: 'http://localhost:3000',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api': ''
+    }
+}));
 
 app.listen(PORT, () => {
     console.log('Express server is live at http://localhost:' + PORT);
