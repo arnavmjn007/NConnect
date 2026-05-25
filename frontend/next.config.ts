c61e155ff9531e822d3119ba997f43db00a23e98
arnavmjn007 <arnavmjn007@gmail.com>
2026-05-26 00:06:23 +0545

    Fixing versions

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.auth0.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com', 
+      },
+      {
+        protocol: 'https',
+        hostname: '*.gravatar.com', 
+      }
+    ],
+  },
   reactCompiler: true,
 };
 
