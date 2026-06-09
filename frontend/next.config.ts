60e67d2e16fd4fc9b8f6da871d67ae73fa3651cc
arnavmjn007 <arnavmjn007@gmail.com>
2026-06-09 23:48:20 +0545

    Adding pathname for cloudinary

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`,
       },
     ],
   },
